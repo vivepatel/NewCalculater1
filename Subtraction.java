19e0369a2ec232f08f5ddca9c15fc8143a1b1109
Vivekpatel <vp20032020@gmail.com>
2025-01-07 13:47:56 +0530

    Subtration funtionality  added

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println("The Subtration of a and b is :"+(a-b));
+    }
+}
