8f117250d5d02dc1e2d221a9d49cf773886518ed
Vivekpatel <vp20032020@gmail.com>
2025-01-07 13:33:16 +0530

    Added  addition functionality added

---
+++ Addition.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println(" The sum of the a and b is :"+(a+b));
+    }
+}
